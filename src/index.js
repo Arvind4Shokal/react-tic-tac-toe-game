c3ca3387a33a2149e042d31d8bd98aee29f809d3
Arvind4Shokal <arvindshokal@gmail.com>
2018-08-14 23:36:32 +0530

    Added double loop to create squares

--- src/index.js
+++ src/index.js
@@ -16,30 +16,29 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
+        key = {i}
         value = {this.props.squares[i]}
         onClick = {() => this.props.onClick(i)}
       />
     );
   }
+  
+  createBoard() {
+    let board = [];
+    for(let i = 0; i < 3; i++) {
+      let children = [];
+      for (let j = 0; j < 3; j++) {
+        children.push(this.renderSquare(3 * i + j))
+      }
+      board.push(<div className="board-row" key={i}>{children}</div>)
+    }
+    return board;
+  }
 
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.createBoard()}
       </div>
     );
   }
